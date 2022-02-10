054a99bbf2b36d065d53773ce1f0cd3de5c25d53
Оля Соколова <Olya.Sokolova.241902@yandex.ru>
2022-02-10 12:50:54 +0300

    Программа, приветствующая пользователя

---
+++ rep/MyHello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tasks
+{
+    class MyHello
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("myhello");
+            string name_user;
+            name_user = Console.ReadLine();
+            Console.WriteLine($"Привет, {name_user}");
+        }
+    }
+}
